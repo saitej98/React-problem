79935f7aa9b8c9a4af0d11e6d4a6172792d76c4c
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-04-16 21:04:15 +0530

    commit

--- vite-project/src/Components/Routes.jsx
+++ vite-project/src/Components/Routes.jsx
@@ -1,10 +1,10 @@
-// import { AddCountry } from "./addCity";
-// import { AddCity } from "./addCountry";
-// import { Home } from "./home";
+import { Routes,Route } from "react-router-dom";
+import { AddCountry } from "./addCity";
+import { AddCity } from "./addCountry";
+import { Home } from "./home";
 
-
-// <Routes>
-//   <Route path="/" element={<Home />} />
-//   <Route path="/add-country" element={<AddCountry />} />
-//   <Route path="/add-city" element={<AddCity />} />
-// </Routes>;
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/add-country" element={<AddCountry />} />
+  <Route path="/add-city" element={<AddCity />} />
+</Routes>;
